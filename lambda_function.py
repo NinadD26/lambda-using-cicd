f64dff5183301d62b5f7d3c57f555c629e803e0e
Ninad <ninad.marvel@gmail.com>
2024-10-17 13:38:38 +0530

    files added

---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+def lambda_handler(event, context):
+
+    # Simulate data processing completed
+
+    message = "lambda cicd zip file testing . "
+
+    return {
+
+        "statusCode": 200,
+
+        "body": message
+    }
+
+ 
